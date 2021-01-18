0cfcb68d08b5e9b83a423f85ec6dc107df70a5a5
최서정 <cchloe2311@daum.net>
2021-01-18 16:00:24 +0900

    added MyContactRepository

---
+++ app/src/main/java/com/example/contentproviderpractice/MyContactsRepository.kt
@@ -0,0 +1,12 @@
+package com.example.contentproviderpractice
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+class MyContactsRepository(private val source: MyContactsDataSource, private val myDispatcher: CoroutineDispatcher) {
+
+    // 다른 예제들은 CursorLoader로 비동기처리하는데 나는 코루틴을 쓸거야
+    suspend fun fetchContacts(): List<MyContact> = withContext(myDispatcher) {
+            source.fetchContacts()
+    }
+}
