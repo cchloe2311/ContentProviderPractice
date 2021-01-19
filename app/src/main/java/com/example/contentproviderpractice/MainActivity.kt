f092cf6f98452260d30d7067de23d497a518af09
cchloe2311 <cchloe2311@daum.net>
2021-01-19 23:38:21 +0900

    added code which checks permission and request it

--- app/src/main/java/com/example/contentproviderpractice/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderpractice/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.contentproviderpractice
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,13 +16,38 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val adapter = MyContactsAdapter()
+    private lateinit var viewModel: MainViewModel
+
+    private val PERMISSIONS_REQUEST_READ_CONTACTS = 1001;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val myViewModelFactory =
-            MyViewModelFactory(application)
-        val viewModel = ViewModelProvider(this, myViewModelFactory).get(MainViewModel::class.java)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        } else {
+            showContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS && grantResults[0] == PackageManager.PERMISSION_GRANTED) showContacts()
+        else Toast.makeText(this, "앱의 주소록 접근 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showContacts() {
+        viewModel =
+            ViewModelProvider(this, MyViewModelFactory(application)).get(MainViewModel::class.java)
+
         parentView.layoutManager = LinearLayoutManager(this)
         parentView.adapter = adapter
         viewModel.myContacts.observe(this, Observer {
--- app/src/main/java/com/example/contentproviderpractice/MyContactsRepository.kt
+++ app/src/main/java/com/example/contentproviderpractice/MyContactsRepository.kt
@@ -7,6 +7,6 @@ class MyContactsRepository(private val source: MyContactsDataSource, private val
 
     // 다른 예제들은 CursorLoader로 비동기처리하는데 나는 코루틴을 쓸거야
     suspend fun fetchContacts(): List<MyContact> = withContext(myDispatcher) {
-            source.fetchContacts()
+        source.fetchContacts()
     }
 }
